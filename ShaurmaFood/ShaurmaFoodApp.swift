55892e2b4212ee9bd2b0813cf18b69c322958339
bakebrlk <210103194@stu.sdu.edu.kz>
2024-02-22 01:06:47 +0600

    addXcode

---
+++ ShaurmaFood/ShaurmaFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShaurmaFoodApp.swift
+//  ShaurmaFood
+//
+//  Created by bakebrlk on 22.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShaurmaFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
