351fcd4ec8725ceff4470f5250b654589735a591
bakebrlk <210103194@stu.sdu.edu.kz>
2024-02-22 03:14:36 +0600

    AddFont

---
+++ ShaurmaFood/model/Font.swift
@@ -0,0 +1,29 @@
+//
+//  Font.swift
+//  ShaurmaFood
+//
+//  Created by bakebrlk on 22.02.2024.
+//
+
+import SwiftUI
+
+struct Font{
+    
+    struct Montserrat {
+        static let ThinItalic = "Montserrat-ThinItalic"
+        static let Thin = "Montserrat-Thin"
+        static let SemiBoldItalic = "Montserrat-SemiBoldItalic"
+        static let SemiBold = "Montserrat-SemiBold"
+        static let MediumItalic = "Montserrat-MediumItalic"
+        static let Medium = "Montserrat-Medium"
+        static let LightItalic = "Montserrat-LightItalic"
+        static let ExtraLight = "Montserrat-ExtraLight"
+        static let ExtraBoldItalic = "Montserrat-ExtraBoldItalic"
+        static let ExtraBold = "Montserrat-ExtraBold"
+        static let BoldItalic = "Montserrat-BoldItalic"
+        static let Bold = "Montserrat-Bold"
+        static let BlackItalic = "Montserrat-BlackItalic"
+        static let Black = "Montserrat-Black"
+        static let Regular = "Montserrat-Regular"
+    }
+}
