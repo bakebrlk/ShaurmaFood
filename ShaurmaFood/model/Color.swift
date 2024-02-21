5fd1eff31d03ff81bb6c07d10b723bba598cb115
bakebrlk <210103194@stu.sdu.edu.kz>
2024-02-22 01:20:47 +0600

    addCustomColor

---
+++ ShaurmaFood/model/Color.swift
@@ -0,0 +1,43 @@
+//
+//  Color.swift
+//  ShaurmaFood
+//
+//  Created by bakebrlk on 22.02.2024.
+//
+
+import SwiftUI
+
+struct CustomColor: Decodable {
+    static let darkRedf = Color(hex: "#B82626")
+    static let brightRed = Color(hex: "#E02222")
+    static let blackishGrey = Color(hex: "#1D1D1D")
+    static let warmWhite = Color(hex: "#FAFAFA")
+    static let white = Color(hex: "#FFFFFF")
+}
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3:
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6:
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8:
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
+}
